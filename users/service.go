107178616dacea43123564a995a4280f87e5940f
Linus Jakobsson <Tejconnect@gmail.com>
2021-01-20 02:45:00 +0100

    chore: remove log

--- users/service.go
+++ users/service.go
@@ -1,14 +1,11 @@
 package users
 
-import "fmt"
-
 type service struct {
 	database Database
 }
 
 func (s service) GetUserSummary(userIds []string) []*User {
 	results := s.database.GetUserSummary(userIds)
-	fmt.Println(results)
 	return results
 }
 
