e4384d334bd387d2607c0d003dc08041f333a05d
Linus Jakobsson <Tejconnect@gmail.com>
2021-01-20 12:10:15 +0100

    chore(config): remove mongo url

--- global/config.go
+++ global/config.go
@@ -6,7 +6,6 @@ type Config struct {
 	AppName string `envconfig:"APP_NAME" default:"user-api"`
 	Env     string `envconfig:"ENV" default:"dev"`
 	Port    string    `envconfig:"PORT" default:"8080""`
-	MongoUrl string `envconfig:"MONGO_URL" default: localhost`
 }
 
 func New() (*Config, error) {
